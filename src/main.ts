e1052158bffcc171cd56df6fbc6a430d3df368a4
Florian Graff <florian.graff7@gmail.com>
2023-06-17 09:19:28 +0200

    added bootstrap 5

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
