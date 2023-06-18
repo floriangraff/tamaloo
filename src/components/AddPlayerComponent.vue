1cf9a23bbd5ca6ba138c39b83d07f93d6808a964
Florian Graff <florian.graff7@gmail.com>
2023-06-18 09:19:08 +0200

    adding players works

--- src/components/AddPlayerComponent.vue
+++ src/components/AddPlayerComponent.vue
@@ -1,18 +1,25 @@
 <template>
   <b>Add Player {{test}}</b><br />
-  <input placeholder="Player Name" v-model="playerName"/>
+  <input placeholder="Player Name" ref="input" v-model="playerName" v-on:keyup.enter="handleAddPlayer"/>
   <button class="btn btn-primary" v-on:click="handleAddPlayer">+</button>
 </template>
 
 <script setup lang="ts">
-defineProps(["playerName"])
+import {ref} from "vue";
 const emit = defineEmits<{
   (e: 'addPlayer', name: String): void
 }>()
-let playerName = ""
+const playerName = ref("")
+const input = ref("input")
 function handleAddPlayer() {
-  console.log("adding player", playerName)
-  emit("addPlayer", playerName)
+  if (playerName.value === "") {
+    return
+  }
+  console.log("adding player", playerName);
+  emit("addPlayer", playerName.value)
+  playerName.value = ""
+  input.value.focus()
+
 }
 </script>
 
